ee2cbe009ce7617b35fdc37b654a3e0e7e5d4636
wittyneko <wittytutu@gmail.com>
2022-03-20 22:57:35 +0800

    upgrade example

--- lib/view_model_binding.dart
+++ lib/view_model_binding.dart
@@ -1,5 +1,4 @@
 import 'package:collection/collection.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
