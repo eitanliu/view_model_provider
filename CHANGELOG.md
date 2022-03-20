e75607c4977587997e025cb4dad1431b1e2fc335
wittyneko <wittytutu@gmail.com>
2022-03-20 22:24:22 +0800

    upgrade provider ^6.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [0.0.5]
+
+* upgrade provider ^6.0.0
+
 ## [0.0.4]
 
 * fix bugs
--- lib/view_model_provider.dart
+++ lib/view_model_provider.dart
@@ -1,7 +1,6 @@
 import 'package:collection/collection.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 import 'package:tuple/tuple.dart';
 
